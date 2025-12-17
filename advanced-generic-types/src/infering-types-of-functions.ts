75a9cc28a60b5c2c9b3e7fe1ef1ed0ece270e638
Mr Punk da Silva <gustavojs417@gmail.com>
2025-12-17 15:00:11 -0300

    add new leans

---
+++ advanced-generic-types/src/infering-types-of-functions.ts
@@ -0,0 +1,13 @@
+import {City, Person, Product, Employee} from "./dataTypes";
+
+type Result<T> = T extends (...args: any) => infer R ? R : never;
+
+function processArray<T,
+    Func extends (T) => any>(data: T[], func: Func): Result<Func>[] {
+    return data.map(item => func(item));
+}
+
+let selectName = (p: Product) => p.name;
+let products = [new Product("Kayak", 275), new Product("Lifejacket", 48.95)];
+let names: string[] = processArray(products, selectName);
+names.forEach(name => console.log(`Name: ${name}`))
