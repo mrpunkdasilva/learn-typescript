c67c514512d8e0fafe641fe93a338e11081fe552
Mr Punk da Silva <gustavojs417@gmail.com>
2026-03-11 23:03:29 -0300

    new beans

--- Writerside/topics/Empty-MD-Topic.md
+++ Writerside/topics/Empty-MD-Topic.md
@@ -1,3 +1,14 @@
 # Empty MD Topic
 
-Start typing here...
+<note>
+
+Link da pasta do projeto:
+
+[https://github.com/mrpunkdasilva/learn-typescript/tree/c0bcbe2b03eb5df940af17d7c6e50162da43e148/usingjs](https://github.com/mrpunkdasilva/learn-typescript/tree/c0bcbe2b03eb5df940af17d7c6e50162da43e148/usingjs)
+
+</note>
+
+
+
+
+
---
+++ usingjs/src/product.ts
@@ -0,0 +1,37 @@
+export class Product {
+
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number
+    ) {
+    }
+
+}
+
+export enum SPORT {
+    Running, Soccer, Watersports, Other
+}
+
+export class SportSProduct extends Product {
+    private _sports: SPORT[];
+
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number,
+        ...sportArray: SPORT[]
+    ) {
+        super(id, name, price);
+        this._sports = sportArray;
+    }
+
+    usedForSport(s: SPORT): boolean {
+        return this._sports.includes(5);
+    }
+
+    get sports(): SPORT[] {
+        return this._sports;
+    }
+}
+
